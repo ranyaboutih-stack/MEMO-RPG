b635824a83e6a83cd6ba23bb46036521ccfe9776
SarahTrml <termoulsarah@yahoo.com>
2026-04-30 23:09:48 +0200

    ajout du personnage

--- main.c
+++ main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include "menu.h"
+#include "structure.h"
 
 int main(){
+
+    Joueur monHeros ;
+    monHeros.ligne = 0 ; 
+    monHeros.colonne = 0 ; 
     afficherMenuAccueil(); 
 
-    int armeChoisie = choisirArme(); 
+    monHeros.armeActive = choisirArme(); 
 
-    printf("tu pars a l'aventure avec l'arme : %d\n", armeChoisie);
+    printf("tu pars a l'aventure avec l'arme : %d\n",monHeros.armeActive);
     
     int actionChoisie = 0; 
     while(actionChoisie !=3){
@@ -16,7 +21,7 @@ int main(){
             printf(" tu avance dans le donjon "); 
         }
         else if(actionChoisie==2){
-            printf("UN MONSTRE APPARAIT ! Tu sors ton arme %d et tu attaque ! " , armeChoisie) ; 
+            printf("UN MONSTRE APPARAIT ! Tu sors ton arme %d et tu attaque ! " , actionChoisie) ; 
 
         }
         else if(actionChoisie ==3 ){
@@ -24,6 +29,6 @@ int main(){
         }
 
     }
-
+     
     return 0 ; 
 }
